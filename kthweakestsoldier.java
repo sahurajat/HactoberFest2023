bf916dbe944f6951912d82f8d679fdb61f169109
Rajat Sahu <119427607+sahurajat@users.noreply.github.com>
2023-10-26 11:57:38 -0400

    Create kthweakestsoldier.java

---
+++ kthweakestsoldier.java
@@ -0,0 +1,58 @@
+import java.util.PriorityQueue;
+
+//wap to find the Kth weakest soldier
+public class eight
+{
+
+    static class Row implements Comparable<Row>
+    {
+        int soldiers;
+        int idx;
+        public Row(int soldiers,int idx)
+        {
+            this.soldiers=soldiers;
+            this.idx=idx;
+        }
+        @Override
+        public int compareTo(Row r2)
+        {
+            //if solders are equal then compare with its idx
+            if(this.soldiers==r2.soldiers)
+            {
+                return this.idx-r2.idx;
+            }
+            else
+            {
+                return this.soldiers-r2.soldiers;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int army[][]={{1,0,0,0},
+                       
+                       {1,0,0,0},
+                       {1,1,1,1},
+                       {1,0,0,0}
+                      };
+
+             int k=2;
+
+          PriorityQueue<Row>pq=new PriorityQueue<>();
+          for(int i=0;i<army.length;i++)
+          {
+            int count=0;
+            for(int j=0;j<army.length;j++)
+            {
+                count +=army[i][j]== 1? 1 : 0 ; 
+            }
+            pq.add(new Row(count, i));
+          }   
+        
+
+          //for finding kth weakest
+          for(int i=0;i<k;i++)
+          {
+            System.out.println("R"+pq.remove().idx);
+          }
+    }
+}
